77a2b8e5ae32be2f59f6f5ba56db8840d550d3cd
Shafin <btech10151.21@bitmesra.ac.in>
2023-10-23 19:53:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const app=express();
+const PORT=5000;
+
+app.use(bodyParser.json());
+// app.use(express.json());
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port: http://localhost:${PORT}`);
+})
+
+
